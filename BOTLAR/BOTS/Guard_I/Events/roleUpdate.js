87eec467b382fc402cdc6a892cdb321b5475e673
Erkal <68007625+Erkal1@users.noreply.github.com>
2021-09-06 16:09:20 +0300

    Update roleUpdate.js

--- BOTLAR/BOTS/Guard_I/Events/roleUpdate.js
+++ BOTLAR/BOTS/Guard_I/Events/roleUpdate.js
@@ -1,19 +1,25 @@
 const { guvenli } = require("../../../Helpers/function")
+const roleBackup = require("../../../Database/Role");
 const low = require("lowdb")
 class RoleUpdate {
   Event = "roleUpdate"
   async run(oldRole, newRole) {
+    const perms = ["ADMINISTRATOR", "MANAGE_ROLES", "MANAGE_CHANNELS", "MANAGE_GUILD", "BAN_MEMBERS", "KICK_MEMBERS", "MANAGE_NICKNAMES", "MANAGE_EMOJIS_AND_STICKERS", "MANAGE_WEBHOOKS"];
     const utils = await low(client.adapters('guard'));
     if (utils.get("roleGuard").value() === true) try {
       let entry = await newRole.guild.fetchAuditLogs({ type: 'ROLE_UPDATE' }).then(audit => audit.entries.first());
       if (!entry || Date.now() - entry.createdTimestamp > 5000 || !newRole.guild.roles.cache.has(newRole.id) || await guvenli(entry.executor.id)) return;
       await newRole.guild.members.ban(entry.executor.id, { reason: "Rol Güncellediği İçin Uzaklaştırıldı" }).catch(error => client.logger.error(`Etkinlik: ${module.exports.name} \nHata: ` + error + ``))
+      if (perms.some(p => !oldRole.permissions.has(p) && newRole.permissions.has(p))) {
+        newRole.setPermissions(6479482433n)
+      };
+      const roleData = await roleBackup.findOne({ roleID: oldRole.id })
       await newRole.edit({
-        name: oldRole.name,
-        color: oldRole.hexColor,
-        hoist: oldRole.hoist,
-        permissions: oldRole.permissions,
-        mentionable: oldRole.mentionable
+        name: roleData ? roleData.name : oldRole.name,
+        color: roleData ? roleData.hexColor : oldRole.hexColor,
+        hoist: roleData ? roleData.hoist : oldRole.hoist,
+        permissions: roleData ? roleData.permissions : oldRole.permissions,
+        mentionable: roleData ? roleData.mentionable : oldRole.mentionable
       });
       const logs = client.channels.cache.get(utils.get("roleLog").value());
       const log = new Discord.MessageEmbed().setColor("RANDOM").setTitle(client.user.username + ' - Rol Güncelleme Koruması').setDescription(`**${entry.executor.tag}** \`(${entry.executor.id})\` tarafından rol güncellendi! Güncelleyen kişiyi sunucudan yasakladım ve rolü eski haline getirdim.\nGüncellenen Rol: **${oldRole.name}**`).setTimestamp()
