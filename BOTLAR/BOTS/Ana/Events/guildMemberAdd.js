217705918148e391228bd9b40a2fb0fdd791182d
Erkal <68007625+Erkal1@users.noreply.github.com>
2021-09-09 10:47:02 +0300

    Update guildMemberAdd.js

--- BOTLAR/BOTS/Ana/Events/guildMemberAdd.js
+++ BOTLAR/BOTS/Ana/Events/guildMemberAdd.js
@@ -8,7 +8,7 @@ class GuildMemberAdd {
       const utils = await low(client.adapters('guard'));
       const setNickname = `${member.user.username.includes(ayarlar.tag) ? ayarlar.tag : (ayarlar.tagsiz ? ayarlar.tagsiz : ayarlar.tag)} İsim | Yaş`;
       const wellcomeChannel = client.channels.cache.get(kanallar.girisKanal)
-      const kontrol = new Date().getTime() - member.user.createdAt.getTime() < ayarlar.userTime ? kontrol = `**Hesap Durumu:** Şüpheli ${emojiler.basarisiz}` : `**Hesap Durumu:** Güvenli ${emojiler.mavionay}`
+      const kontrol = new Date().getTime() - member.user.createdAt.getTime() < ayarlar.userTime ? `**Hesap Durumu:** Şüpheli ${emojiler.basarisiz}` : `**Hesap Durumu:** Güvenli ${emojiler.mavionay}`
       const tag = utils.get("TagliAlim").value() ? `Tagımızı \`${ayarlar.tag}\` alarak kayıt olabilirsin!` : `Tagımızı \`${ayarlar.tag}\` alarak bizlere destek olabilirsin!`
       await member.setNickname(`${setNickname}`)
       if (wellcomeChannel) {
