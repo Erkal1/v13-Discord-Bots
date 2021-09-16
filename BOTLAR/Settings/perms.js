1b719dfca513011dbffc2df365db31bcf269bdc4
Erkal <68007625+Erkal1@users.noreply.github.com>
2021-09-16 15:57:43 +0300

    Update perms.js

--- BOTLAR/Settings/perms.js
+++ BOTLAR/Settings/perms.js
@@ -1,8 +1,8 @@
 module.exports = {
    Perms: [
       { permissions: [roller.clownHammer], commands: "vip", roles: roller.vipRol },
-      { permissions: [roller.clownHammer], commands: "müzisyen", roles: "878239701939675167" },
-      { permissions: [roller.clownHammer], commands: "Vokalist", roles: "815844799881936936" },
-      { permissions: [roller.clownHammer], commands: "dj", roles: "815844799185420308" }
+      { permissions: [roller.clownHammer], commands: "müzisyen", roles: "" },
+      { permissions: [roller.clownHammer], commands: "Vokalist", roles: "" },
+      { permissions: [roller.clownHammer], commands: "dj", roles: "" }
    ]
 }
